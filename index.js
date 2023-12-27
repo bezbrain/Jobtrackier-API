41163dc29d1562b24ec70268cd26f41abe0e1caf
bezbrain <belloquadriolawale@gmail.com>
2023-12-27 15:36:57 +0100

    Set up the api security

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 require("dotenv").config();
 require("express-async-errors");
+
+// Extra security
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 const authMiddleware = require("./middleware/auth");
 const authRouter = require("./routes/auth.route");
 const jobRouter = require("./routes/job.route");
@@ -10,6 +17,19 @@ const connectDB = require("./db/connect");
 const app = express();
 
 app.use(express.json());
+
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+    // store: ... , // Use an external store for consistency across multiple server instances.
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // The dynamic port
 const port = process.env.PORT || 3000;
