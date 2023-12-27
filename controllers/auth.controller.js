4d14aa375dc971f316c4b48f679ac0d8c2d82426
bezbrain <belloquadriolawale@gmail.com>
2023-12-27 13:01:03 +0100

    Implemented the login user auth and handle all server errors accordingly

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,9 +1,11 @@
 const UserCollection = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
+const BadRequestError = require("../errors/bad-request");
+const UnauthenticatedError = require("../errors/unauthenticated");
 
 const signup = async (req, res) => {
   const user = await UserCollection.create(req.body);
-
+  // Fetch token
   const token = user.createJWT();
 
   res.status(StatusCodes.CREATED).json({
@@ -14,7 +16,37 @@ const signup = async (req, res) => {
 };
 
 const signin = async (req, res) => {
-  res.send("Please Login");
+  const {
+    body: { email, password },
+  } = req;
+
+  //   Check if email and password values are provided
+  if (!email || !password) {
+    throw new BadRequestError("Email and password must be provided");
+  }
+
+  //   Find one email value provided by the user in the dbb
+  const user = await UserCollection.findOne({ email });
+
+  if (!user) {
+    throw new BadRequestError("Email does not exist");
+  }
+
+  //   Fetch password comparison
+  const isPasswordCorrect = await user.comparePassword(password);
+  // Check if password is correct
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("Password does not match");
+  }
+
+  //   Fetch token
+  const token = user.createJWT();
+
+  res.status(StatusCodes.CREATED).json({
+    success: true,
+    message: "Login successful",
+    token,
+  });
 };
 
 module.exports = {
--- models/User.js
+++ models/User.js
@@ -46,4 +46,10 @@ UserSchema.methods.createJWT = function () {
   );
 };
 
+// Compare password
+UserSchema.methods.comparePassword = async function (userPassword) {
+  const isMatch = await bcrypt.compare(userPassword, this.password);
+  return isMatch;
+};
+
 module.exports = mongoose.model("Users", UserSchema);
